8219a9fac697fcf4cc52e148584e8bfbcfdb2b65
maltiez2 <maltiez2@gmail.com>
2026-06-13 11:47:18 +0300

    wip

---
+++ source/Utils/ParsingUtils.cs
@@ -0,0 +1,32 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+
+namespace OverhaulLib.Utils;
+
+public static class ParsingUtils
+{
+    public static TObject? LoadObjectFromFile<TObject>(IAsset asset, ICoreAPI? api = null, object? caller = null, Type? type = null) where TObject : class
+    {
+        TObject? fileConent = null;
+        try
+        {
+            fileConent = asset.ToObject<TObject>();
+        }
+        catch (Exception exception)
+        {
+            if (caller != null)
+            {
+                Log.Error(api, caller, $"Failed to load object from '{asset.Location}'. Exception: {exception}");
+            }
+            else if (type != null)
+            {
+                Log.Error(api, type, $"Failed to load object from '{asset.Location}'. Exception: {exception}");
+            }
+            else
+            {
+                Log.Error(api, typeof(ParsingUtils), $"Failed to load '{nameof(TObject)}' from '{asset.Location}'. Exception: {exception}");
+            }
+        }
+        return fileConent;
+    }
+}
